22bf244d41121107bd260c362f112f8cc4e1733b
Moritz Göckel <moritz.goeckel@gmail.com>
2017-11-04 00:48:33 +0100

    Live trader

---
+++ src/com/moritzgoeckel/Live/Trader.java
@@ -0,0 +1,36 @@
+package com.moritzgoeckel.Live;
+
+import com.moritzgoeckel.Data.Candle;
+import com.moritzgoeckel.Market.BacktestMarket;
+import com.moritzgoeckel.Market.Market;
+import com.moritzgoeckel.Strategy.Strategy;
+import com.moritzgoeckel.Strategy.StrategyDNA;
+
+import java.util.List;
+
+public class Trader {
+
+    private Strategy strategy;
+
+    public Trader(StrategyDNA dna, List<Candle> preparationCandles) throws IllegalAccessException, InstantiationException {
+        strategy = dna.getStrategyLogic().newInstance();
+        BacktestMarket backtestMarket = new BacktestMarket();
+
+        for(Candle c : preparationCandles){
+            backtestMarket.updateCandle(c);
+            strategy.candleCompleted(c, backtestMarket);
+        }
+    }
+
+    private String lastCandleDate = "";
+    public void doCandle(Candle c, Market market){
+        if(lastCandleDate.equals(c.getTime().toString()))
+            throw new RuntimeException("Candle already processed");
+
+        if(!c.getComplete())
+            throw new RuntimeException("Candle not yet completed");
+
+        lastCandleDate = c.getTime().toString();
+        strategy.candleCompleted(c, market);
+    }
+}
